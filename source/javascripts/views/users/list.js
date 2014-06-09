a1e78c15116b430b387b4a2d879d399930421d8e
badmanski <stanislav.gorski@gmail.com>
2014-06-09 22:58:08 +0300

    Render template on users collection fetch

--- source/javascripts/views/users/list.js
+++ source/javascripts/views/users/list.js
@@ -1,6 +1,17 @@
+var Users = Backbone.Collection.extend({
+  url: '/users'
+});
+
 var UserList = Backbone.View.extend({
   el: '.list',
   render: function() {
-    this.$el.html('Users list');
+    var scope = this;
+    var users = new Users();
+    users.fetch({
+      success: function(data) {
+        var template = _.template($('#user-list-template').html(), data);
+        scope.$el.html(template);
+      }
+    });
   }
 });
