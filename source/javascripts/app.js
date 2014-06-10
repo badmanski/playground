d0002c37ced8c42d9350352b11395b8d773da644
badmanski <stanislav.gorski@gmail.com>
2014-06-11 00:08:50 +0300

    Add serializeObject helper

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -2,6 +2,22 @@ $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
   options.url = 'http://node.dev:3000' + options.url
 });
 
+$.fn.serializeObject = function() {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+    if (o[this.name] !== undefined) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
+}
+
 var users = new UserList();
 var router = new Router();
 
